6877c681ce0be6c5d77237925ff1a92350a2f0b4
jonatas-valete <jonatasfelippe@gmail.com>
2024-08-03 10:09:53 -0300

    operadores logicos incrementado

--- operadores_logicos.cpp
+++ operadores_logicos.cpp
@@ -5,15 +5,30 @@ using namespace std;
 // Operadores Lógicos: E: &&, ou:||, Não: !
 /* As condições devem ser feitas dentro do parentese de cada if por exemplo. */
 
-string carro, cor;
-cout << "digite a marca do carro: " << endl;
-cin >> carro;
+void is_chevrolet(string carro, string cor){
+         if (carro == "chevrolet" && cor == "preto" || cor == "branco"){
+             cout << "Valor R$ 120.000,00" << endl;
+         }
+}
 
-cout << "digite a cor do carro: " << endl;
-cin >> cor;
+void is_mercedes(string carro, string cor){
+         if (carro == "mercedes" && cor == "preto" || cor == "branco"){
+             cout << "Valor R$ 300.000,00" << endl;
+         }   
+}
 
-if (carro == "chevrolet" && cor == "preto"){
-    cout >> "valor: R$ 120.000,00" << endl;
-}else{
-    cout << "carro nao encontrado\n";
+
+
+int main(){
+    string carro, cor;
+
+    cout << "digite a marca do carro: " << endl;
+    cin >> carro;
+
+    cout << "digite a cor do carro: " << endl;
+    cin >> cor;
+
+    is_chevrolet(carro, cor);
+    is_mercedes(carro, cor);
+    return 0;
 }
